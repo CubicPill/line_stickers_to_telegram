c5a49fecb4dbd773c571d61ce1dd505de7292739
CubicPill <huazhengchang97@gmail.com>
2019-01-17 00:32:04 +0800

    to_gif

--- processing.py
+++ processing.py
@@ -1,3 +1,5 @@
+import os
+import subprocess
 from enum import Enum
 from threading import Thread
 
@@ -44,14 +46,34 @@ def scale_image(in_file, out_file):
 
 def to_gif(in_file, out_file):
     input_stream = ffmpeg.input(in_file, f='apng')
+    temp_apng_filename = 'tmp' + os.path.sep + 'temp_' + in_file.split(os.path.sep)[-1]
+    temp_palette_filename = 'tmp' + os.path.sep + 'temp_palette_' + in_file.split(os.path.sep)[-1]
+    input_stream \
+        .filter('pad', w='iw*2', h='ih', x='iw', y='ih', color='white') \
+        .crop(width='iw/2', height='ih', x=0, y=0) \
+        .overlay(input_stream) \
+        .output(temp_apng_filename, f='apng', plays=0) \
+        .overwrite_output() \
+        .run()
+    input_stream.filter('palettegen').output(temp_palette_filename) \
+        .overwrite_output() \
+        .run()
+    ffmpeg.input(temp_apng_filename).output(out_file, f='gif') \
+        .overwrite_output() \
+        .run()
+    # currently I have no idea how to do this in ffmpeg-python
+    subprocess.Popen(
+        ['ffmpeg', '-i', temp_apng_filename, '-i', temp_palette_filename, '-lavfi', 'paletteuse', '-y', out_file])
+
+
+def to_video(in_pic, in_audio, out_file):
+    input_stream = ffmpeg.input(in_pic, f='apng')
+    if in_audio:
+        pass
     input_stream \
         .filter('pad', w='iw*2', h='ih', x='iw', y='ih', color='white') \
         .crop(width='iw/2', height='ih', x=0, y=0) \
         .overlay(input_stream, format='rgb') \
-        .output(out_file, f='gif') \
+        .output(out_file, f='mp4') \
         .overwrite_output() \
         .run()
-
-
-def to_video(in_pic, in_audio, out_file):
-    pass
